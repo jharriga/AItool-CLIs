62a8ab24e89ded6fe91e75d3e9e21e8cf44814b6
John Harrigan <jharriga@redhat.com>
2025-10-31 13:59:24 -0400

    Add example_cursor_agent.sh

--- Cursor-CLI/example_cursor_agent.sh
+++ Cursor-CLI/example_cursor_agent.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cursor-agent -p "Generate a Dockerfile which uses UBI-9 as the base image and echos 'Hello World' to the console."
 podman build -t hello-world .
 podman run hello-world
